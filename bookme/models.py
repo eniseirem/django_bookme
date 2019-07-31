af0ec9bfba3aaaa20bb9116755a6861236f375dd
eniseirem <eniseirem@gmail.com>
2019-08-01 00:51:23 +0300

    database model created

--- bookme/models.py
+++ bookme/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Booklist(models.Model):
+    ISBN = models.CharField(max_length=150)
+    title = models.TextField()
+    image = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
